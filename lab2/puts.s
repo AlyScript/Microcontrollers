63632c2edbc8fb8aa5af892f0e7995fb8a055f05
Adam Aly <adam.aly@student.manchester.ac.uk>
2025-02-10 10:33:25 +0000

    Fixing bit patterns that werre the wrong way round

--- lab2/puts.s
+++ lab2/puts.s
@@ -23,22 +23,24 @@ CONTROL  EQU 0x0001_0101
 puts
 	; /// Step 1 \\\
 
-	lw t0, CONTROL					; read what is in the control already
-	li t1, 0b1011					; to set RS to 0 (this might be the wrong way round, I am using little endian)
-	li t2, 0b0010 					; to set R/W to 1 (also might be the wrong way round)
+	lbu t0, CONTROL					; read what is in the control already
+	li t1, 0b1101					; to set RS to 0 (this might be the wrong way round, I am using little endian)
+	li t2, 0b0001 					; to set R/W to 1 (also might be the wrong way round)
 	and t0, t1, t0					; set RS  to 0 
 	or t0, t2, t0					; set R/W to 1
-	sw t0, CONTROL, t4				; write back to control with correct bits set (t3 must be clear!)
+	li t4, 0
+	sb t0, CONTROL, t4				; write back to control with correct bits set (t3 must be clear!)
 
 	j loop
 
 loop
 	; /// Step 2 \\\
 
-	lw t0, CONTROL					; read what is in the control already
+	lb t0, CONTROL					; read what is in the control already
 	li t3, 0b0100					; to set E to 1
 	or t0, t3, t0					; set E to 1
-	sw t0, CONTROL, t4				; write back to control with correct bits set (t3 must be clear!)
+	li t4, 0
+	sb t0, CONTROL, t4				; write back to control with correct bits set (t3 must be clear!)
 
 	; /// Step 2a \\\
 
@@ -50,17 +52,18 @@ loop
 	; Read LCD Status Byte (Busy Flag) -- this is bit 7 of the data bus
 	; We put the value of this into t5
 
-	lw t0, DATA_BUS
-	li t5, 0b0000_0001				
+	lb t0, DATA_BUS
+	li t5, 0b1000_0000				
 	and t5, t0, t5
 
 	; /// Step 4 \\\
 
 	; disable bit 2 of control
-	lw t0, CONTROL
-	li t2, 0b1101					; to set E to 0
+	lb t0, CONTROL
+	li t2, 0b1011					; to set E to 0
 	and t1, t2, t1
-	sw t1, CONTROL, t4				; write back to control with correct bits set (t4 must be clear!)
+	li t4, 0
+	sb t1, CONTROL, t4				; write back to control with correct bits set (t4 must be clear!)
 	
 	; /// Step 5 \\\
 	; for a 1200 ns delay, we need 12 iterations of the delay loop
@@ -77,13 +80,14 @@ loop
 ; /// Step 7 \\\
 ; Carry out the write
 write
-	lw t0, CONTROL
-	li t1, 0b0111				; to set R/W to 0	
-	li t2, 0b0100               ; to set RS to 1 
+	lb t0, CONTROL
+	li t1, 0b1110				; to set R/W to 0	
+	li t2, 0b0010               ; to set RS to 1 
 
 	and t1, t0, t1				; for RW
 	or t1, t1, t2				; for RS
-	sw t1, CONTROL, t4			; write back to control with correct bits set (t4 must be clear!)
+	li t4, 0
+	sb t1, CONTROL, t4			; write back to control with correct bits set (t4 must be clear!)
 
 	; /// Step 8 \\\
 	; Now to output the data (character) to the data bus
@@ -91,10 +95,11 @@ write
 
 	; /// Step 9 \\\
 	; Enable the bus
-	lw t0, CONTROL
-	li t1, 0b0010 
+	lb t0, CONTROL
+	li t1, 0b0100 
  	or t1, t0, t1
-	sw t1, CONTROL, t4
+	li t4, 0
+	sb t1, CONTROL, t4
 
 	; /// Step 9a \\\
 	; Delay for 500 ns
@@ -103,10 +108,11 @@ write
 
 	; /// Step 10 \\\
 	; Disable the bus by setting E to 0
-	lw t0, CONTROL
-	li t1, 0b1101 
+	lb t0, CONTROL
+	li t1, 0b1011 
 	and t1, t0, t1
-	sw t1, CONTROL, t4
+	li t4, 0
+	sb t1, CONTROL, t4
 
 	ret
 
