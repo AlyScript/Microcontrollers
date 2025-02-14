b4ea5f0754b66f6c904ba25be40e1fbf4362e040
Adam Aly <adam.aly@student.manchester.ac.uk>
2025-02-14 19:05:22 +0000

    feat(puts.s): printing is now fully functional
    - The issue was that I was using SW to store at the control and data
    - The padding was therefore causing bits to be overwritten meaning that
      the control and data bus were not being written to!
    
    NOTE TO SELF: When dealing with byte addressable memory, use SB and LB
    (obviously!)

--- lab2/puts.s
+++ lab2/puts.s
@@ -2,7 +2,6 @@ ORG 0
 
 start
 
-
 	la sp, STACK
 	la s2, STR              ; s2 is a pointer to the string
 	
@@ -27,22 +26,22 @@ puts
 	addi sp, sp, -4
 	sw ra, [sp]
 
-	lw t0, CONTROL					; read what is in the control already
+	lbu t0, CONTROL					; read what is in the control already
 	li t1, 0b1101					; to set RS to 0 (this might be the wrong way round, I am using little endian)
 	li t2, 0b0001 					; to set R/W to 1 (also might be the wrong way round)
 	and t0, t1, t0					; set RS  to 0 
 	or t0, t2, t0					; set R/W to 1
 	li t4, 0
-	sw t0, CONTROL, t4				; write back to control with correct bits set (t3 must be clear!)
+	sb t0, CONTROL, t4				; write back to control with correct bits set (t3 must be clear!)
 
 loop
 	; /// Step 2 \\\
 
-	lw t0, CONTROL					; read what is in the control already
+	lbu t0, CONTROL					; read what is in the control already
 	li t3, 0b1100					; to set E to 1 (and turn backlight on)
 	or t0, t3, t0					; set E to 1
 	li t4, 0
-	sw t0, CONTROL, t4				; write back to control with correct bits set (t4 must be clear!)
+	sb t0, CONTROL, t4				; write back to control with correct bits set (t4 must be clear!)
 
 	; /// Step 2a \\\
 
@@ -54,18 +53,18 @@ loop
 	; Read LCD Status Byte (Busy Flag) -- this is bit 7 of the data bus
 	; We put the value of this into t5
 
-	lw t0, DATA_BUS
+	lbu t0, DATA_BUS
 	li t5, 0b1000_0000				
 	and t5, t0, t5
 
 	; /// Step 4 \\\
 
 	; disable bit 2 of control
-	lw t0, CONTROL
+	lbu t0, CONTROL
 	li t2, 0b1011					; to set E to 0
 	and t0, t2, t0
 	li t4, 0
-	sw t0, CONTROL, t4				; write back to control with correct bits set (t4 must be clear!)
+	sb t0, CONTROL, t4				; write back to control with correct bits set (t4 must be clear!)
 	
 	; /// Step 5 \\\
 	; for a 1200 ns delay, we need 12 iterations of the delay loop
@@ -80,27 +79,27 @@ loop
 
 	; /// Step 7 \\\
 	; Carry out the write
-	lw t0, CONTROL
+	lbu t0, CONTROL
 	li t1, 0b1110				; to set R/W to 0	
 	li t2, 0b0010               ; to set RS to 1 
 
 	and t0, t0, t1				; for RW
 	or t0, t0, t2				; for RS
 	li t4, 0
-	sw t0, CONTROL, t4			; write back to control with correct bits set (t4 must be clear!)
+	sb t0, CONTROL, t4			; write back to control with correct bits set (t4 must be clear!)
 
 	; /// Step 8 \\\
 	; Now to output the data (character) to the data bus
 	li t4, 0
-	sw s0, DATA_BUS, t4
+	sb s0, DATA_BUS, t4
 
 	; /// Step 9 \\\
 	; Enable the bus
-	lw t0, CONTROL
+	lbu t0, CONTROL
 	li t1, 0b0100 
  	or t0, t0, t1
 	li t4, 0
-	sw t0, CONTROL, t4
+	sb t0, CONTROL, t4
 
 	; /// Step 9a \\\
 	; Delay for 500 ns
@@ -109,11 +108,11 @@ loop
 
 	; /// Step 10 \\\
 	; Disable the bus by setting E to 0
-	lw t0, CONTROL
+	lbu t0, CONTROL
 	li t1, 0b1011 
 	and t0, t0, t1
 	li t4, 0
-	sw t0, CONTROL, t4
+	sb t0, CONTROL, t4
 
 	lw ra, [sp]
 	addi sp, sp, 4
@@ -153,8 +152,8 @@ done
 
 STR DEFB "Hello World!\0"
 ALIGN
-DATA_BUS DEFW 0x0001_0100	
-CONTROL  DEFW 0x0001_0101	
+DATA_BUS EQU 0x0001_0100	
+CONTROL  EQU 0x0001_0101	
 ;ALIGN							; Align the stack to 4 bytes (this is probably not necessary since we are already aligned?)
 STACK_END DEFS 100 				; Reserve 100 bytes for the stack and point to the end (this is a stack `size` of 25, since each `item` is a word...)
 STACK
