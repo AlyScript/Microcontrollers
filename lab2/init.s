516cd07dc5ea5801442c525fab2d6c1e7a762afb
adam <adamaly93@gmail.com>
2025-02-18 12:59:54 +0000

    refactor(init.s): update stack pointer handling and trap vector setup

--- lab2/init.s
+++ lab2/init.s
@@ -6,21 +6,23 @@ INCLUDE puts.s
 ; ------------ Machine Mode Initialisation -----------------
 
 init
-    ; Set the stack pointer
-    la sp, MSTACK
-    csrw mscratch, sp
-
-    ; Set the trap vector
-    la t0, TRAP_VECTOR
-    csrw mtvec, t0
-
     ; Clear MPP to set U-Mode
     li t0, MPP_BITMASK
     csrc mstatus, t0
+    
+    ; Set the trap vector
+    la t0, MHANDLER
+    csrw mtvec, t0
+    
+    ; Copy `machine` SP for use in handler
+    csrw mscratch, sp
+
+    ; Change SP to user stack
+    ; ---- Where do we get the location of the user stack though? ----
 
     ; Set MEPC to the start of our user program
-    li t0, MAIN_START
-    csrw mepc, t0
+    la ra, MAIN_START
+    csrw mepc, ra
 
     mret
 
@@ -28,11 +30,11 @@ init
 MSTACK_END DEFS 100 				; Reserve 100 bytes for the stack and point to the end (this is a stack `size` of 25, since each `item` is a word...)
 MSTACK
 
-TRAP_VECTOR EQU 0x00000200
+MHANDLER EQU 0x0000_0200
 
 ; Use CSRC to clear MPP (i.e. for U-Mode)
 ; Use CSRS to set MPP (i.e. for M-Mode)
-MPP_BITMASK   EQU 0x00001800
+MPP_BITMASK   EQU 0x0000_1800
 
 ; Main program start address
-MAIN_START EQU 0x00040000
+MAIN_START EQU 0x0004_0000
