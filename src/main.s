5e6c7dc2f9a6b002573c3e2603468f949c77ccb7
alyscript <adamaly93@gmail.com>
2025-02-18 14:27:39 +0000

    Refactor main.s: Update comments regarding user stack

--- src/main.s
+++ src/main.s
@@ -2,6 +2,6 @@ ORG 0x00040000
 
 la sp, STACK
 
-; Machine Stack
+; User Stack
 STACK_END DEFS 100 				; Reserve 100 bytes for the stack and point to the end (this is a stack `size` of 25, since each `item` is a word...)
 STACK
